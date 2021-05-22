3aec38db6afd1e5a8733ff2c5f3bdf2c0c95beda
mksmp <71922124+mksmp@users.noreply.github.com>
2021-05-22 12:57:05 +0300

    Add files via upload

--- NeuralNetwork/Interface/backendclient.cpp
+++ NeuralNetwork/Interface/backendclient.cpp
@@ -1,5 +1,5 @@
 #include "backendclient.h"
-
+#include "vector"
 
 bool check(std::string pass)
 {
@@ -19,4 +19,26 @@ bool check(std::string pass)
     {
         return false;
     }
+}
+
+bool check_of_length(std::string collayers, std::string colneurallayers)
+{
+    int layers = stoi(collayers);
+    std::vector<int>neurons_on_layers;
+    while(colneurallayers.size() > 0)
+    {
+        int pos = colneurallayers.find("&");
+        neurons_on_layers.push_back(stoi(colneurallayers.substr(0,pos)));
+        colneurallayers.erase(0,pos+1);
+    }
+    if (layers == neurons_on_layers.size())
+    {
+        for (int i = 0; i < layers; i++) // проверка на кол-во нейронов в слое, 2<layers<15
+        {
+            if (neurons_on_layers[i] < 2 or neurons_on_layers[i] > 15)
+                return false;   
+        }
+        return true;
+    }
+    else return false;
 }
--- NeuralNetwork/Interface/backendclient.h
+++ NeuralNetwork/Interface/backendclient.h
@@ -3,8 +3,7 @@
 #include <string>
 
 bool check(std::string pass);
-
-
+bool check_of_length(std::string collayers, std::string colneurallayers);
 
 
 #endif // BACKENDCLIENT_H
