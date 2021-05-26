bd37502941e6efd256c6e7c19178ac7838e11a7f
Dinar Ganiev <71919502+DinarGaniev@users.noreply.github.com>
2021-05-26 15:14:41 +0300

    Add files via upload

---
+++ InterfaceOp/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['failauth_2',['FailAuth',['../class_window.html#a145ae4d2987da6168a05c076b5012f62',1,'Window']]],
+  ['failreg_3',['FailReg',['../class_window.html#a94ef1690898d2ee7507bc6d5a3a678a8',1,'Window']]]
+];
---
+++ InterfaceOp/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mytcpclient_4',['MyTcpClient',['../class_window.html#a94ecff3c9b7c21e512ab71474ce0a5c6',1,'Window']]]
+];
